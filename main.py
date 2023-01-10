41ff0fda066435e03b75743708940c0fc1b35ac6
Daniel Kimutai <daniellkimutai@gmail.com>
2023-01-10 23:06:58 +0300

    Leap Year Calculator V 1.0

--- main.py
+++ main.py
@@ -8,9 +8,16 @@
 print("Welcome to the Leap Year Checking Application\n")
 checkYear = float(input("Please input the year that you want to check: "))
 
-div_by_four = checkYear % 4
-div_by_hundred = checkYear % 100
-div_by_fourhundred = checkYear % 400
+# div_by_four = checkYear % 4
+# div_by_hundred = checkYear % 100
+# div_by_fourhundred = checkYear % 400
 
-
+if checkYear % 4 != 0 :
+    print("This is not a leap year!")
+elif checkYear % 100 != 0:
+    print("This is a leap year!")
+elif checkYear % 400 != 0:
+    print("This is not leap year!")
+else:
+    print("Your input is a leap year!")
 
